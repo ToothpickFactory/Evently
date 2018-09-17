45e43ff3428bcaa07695e2e4aa1bb0741be48c59
Austin Lovell <alovell@goreact.com>
2018-09-17 15:55:58 -0600

    added more commands

--- api-server/deploy.sh
+++ api-server/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ./.env
-ssh root@$IP_ADDRESS "cd $EVENTLY_PATH && git pull && cd api-server && npm install && npm start"
+ssh root@$IP_ADDRESS "cd $EVENTLY_PATH && git pull && cd api-server && npm install && npm run reload"
