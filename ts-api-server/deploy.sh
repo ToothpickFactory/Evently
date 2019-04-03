64bd033649155cecb974a3268e56618460c2f485
toothpickfactory <austin.d.lovell@gmail.com>
2019-04-02 21:47:46 -0600

    added deploy script

--- ts-api-server/deploy.sh
+++ ts-api-server/deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 source ./.env
-ssh root@$IP_ADDRESS "cd $EVENTLY_PATH && git pull && cd api-server && npm install && npm run reload"
+zip -r api-server.zip dist package.json
+scp api-server.zip root@$IP_ADDRESS:~/
+rm api-server.zip
+ssh root@$IP_ADDRESS "cd ~ && unzip -o api-server.zip -d api-server/ && chmod -R 755 api-server && rm api-server.zip && cd api-server && npm install --production && npm run reload"
